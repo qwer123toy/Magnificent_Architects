c8ec220bde64e9117d20e826a2a15ece8462666c
LeeKangHo1 <kangho.lee.pr@gmail.com>
2024-08-22 12:48:02 +0900

    오후 12시 47분 깃허브 초기 세팅 완료

--- 240822 team1MagnifientArchitecs/src/_notice.java
+++ 240822 team1MagnifientArchitecs/src/_notice.java
@@ -3,7 +3,7 @@ public class _notice {
 	// 8월 22일 팀 1 team1 Magnifient Architecs 가상 주식 투자 프로그램
 //	브랜치
 // 	main -> 배포용, 최대한 덜 사용
-//	master -> 모든 브랜치에 권한
+//	master -> 브랜치 병합용
 //	goho -> 구예은
 //	gohu -> 이제민
 //	gohi -> 이강호
