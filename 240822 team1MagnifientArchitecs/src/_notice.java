c913f1cb6a36aa2c85d50882c2ab7c752be7d177
LeeKangHo1 <kangho.lee.pr@gmail.com>
2024-08-26 10:15:37 +0900

    <conf> main브랜치 삭제, 배표용 release 브랜치 생성

--- 240822 team1MagnifientArchitecs/src/_notice.java
+++ 240822 team1MagnifientArchitecs/src/_notice.java
@@ -3,7 +3,7 @@ public class _notice {
 	// 8월 22일 팀 1 team1 Magnifient Architecs 가상 주식 투자 프로그램
 //	브랜치
 
-//	main -> 배포용, 최대한 덜 사용
+//	release -> 배포용, 최대한 덜 사용
 //	master -> 모든 브랜치 합병용
 //	goho -> 구예은
 //	gohu -> 이재민
@@ -12,5 +12,7 @@ public class _notice {
 	// 8월 26일
 //	좋은 아침입니다.
 //	dbUtil 패키지에 DBUtil.java와 go_db.propertis 파일을 추가했으니 db 접속 가능합니다.
+	
+//	main 브랜치 삭제하고 배포용 release 브랜치 생성. 
 	
 }
